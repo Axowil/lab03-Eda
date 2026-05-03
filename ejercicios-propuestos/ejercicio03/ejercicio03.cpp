554ba171813d32d3c3e3893b927f68210554e56d
Axowil <welfordwillian@gmail.com>
2026-05-03 09:37:52 -0500

    terminando lab03 eda

---
+++ ejercicios-propuestos/ejercicio03/ejercicio03.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n = 5 ; 
+    int* arr = new int[n];
+
+    int i = 0 ;
+    while (i<5){
+        arr[i] = i=1;
+        i++;
+    }
+    
+    //imprimir el resultado 
+    int j = 0 ;
+    while (j<n)
+    {
+        cout << arr[j] << " ";
+        j++;
+    }
+    delete arr;
+    return 0;
+    
+}
